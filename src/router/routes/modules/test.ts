df54203ec2f7ccaf2e0ddf98a35b5e92e7f6ced3
wangxin <vino0908@outlook.com>
2023-05-24 17:35:27 +0800

    chore: 删除测试代码

--- src/router/routes/modules/test.ts
+++
@@ -1,9 +0,0 @@
-import type {AppRouteRecordRaw} from "../../types"
-
-const test: AppRouteRecordRaw = {
-  path: '/test',
-  name: 'test',
-  meta: {},
-  component: () => (import("../../../views/LoginTest.vue"))
-}
-export default test

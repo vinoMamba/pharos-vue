a248edefc308e57b734a373377b89fe4014e0e12
vino <vino0908@outlook.com>
2023-05-30 23:05:57 +0800

    feat: 封装一层useMessage

--- src/hooks/web/useMessage.ts
+++ src/hooks/web/useMessage.ts
@@ -1,7 +1,42 @@
-import {message} from "ant-design-vue";
+import {message, Modal, notification, type ModalFuncProps} from "ant-design-vue";
+
+function createSuccessModal(options: ModalFuncProps) {
+  return Modal.success({
+    ...options,
+  })
+}
+function createErrorModal(options: ModalFuncProps) {
+  return Modal.error({
+    ...options,
+  })
+}
+
+function createWarningModal(options: ModalFuncProps) {
+  return Modal.warning({
+    ...options,
+  })
+}
+
+function createInfoModal(options: ModalFuncProps) {
+  return Modal.info({
+    ...options,
+  })
+}
+function createConfirmModal(options: ModalFuncProps) {
+  return Modal.confirm({
+    ...options,
+  })
+}
+
 
 export function useMessage() {
   return {
-    message
+    message,
+    createSuccessModal,
+    createErrorModal,
+    createWarningModal,
+    createInfoModal,
+    createConfirmModal,
+    notification
   }
 }
